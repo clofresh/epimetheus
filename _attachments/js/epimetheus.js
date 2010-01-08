ef486b927bb9868bfef1f6a7eb09b75a6553d2d6
Carlo Cabanilla <carlo.cabanilla@gmail.com>
2010-01-07 23:44:24 -0500

    switch from json-template to mustache.js

--- _attachments/js/epimetheus.js
+++ _attachments/js/epimetheus.js
@@ -4,50 +4,69 @@ $(document).ready(function() {
                 + "?" + $("#search_form").serialize();
     $.getJSON(url, 
       function(data) {
+        var updates = [
+          "search_results_metadata",
+          "search_results",
+          "search_pager"
+        ];
         
-        $("#search_results_metadata").html(new jsontemplate.Template(
-          "Results {from} - {to} of {total_rows} ({duration} seconds)"
-        ).expand({
-          from:       data.skip + 1,
-          to:         Math.min(data.skip + data.limit, data.total_rows),
-          total_rows: data.total_rows,
-          duration:   ((data.search_duration 
-                          + data.fetch_duration) / 1000).toFixed(3)
-        }));
-        
-        $("#search_results").html(new jsontemplate.Template(
-          ["{.repeated section rows}",
-           "  <div>",
-           "    {fields.timestamp} - {fields.default}",
-           "  </div>",
-           "{.end}"].join("\n")
-        ).expand(data));
-        
-        var num_pages = Math.ceil(data.total_rows / data.limit);
-        var current_page = parseInt(data.skip / data.limit) + 1;
-        
-        $("#search_pager").html(
-          _(_.range(num_pages)).map(function(i) {
-              var page_num = i + 1;
+        _(updates).map(function(section) { 
+          $("#" + section).html(epi[section](data)) 
+        });
 
-              if (page_num == current_page) {
-                return page_num;
-
-              } else {
-                var lambda = ["return function() {",
-                                "$('#search_form')[0].skip.value = ",
-                                i,
-                                " * $('#search_form')[0].limit.value; ",
-                                "$('#search_form').trigger('submit');",
-                                "return false;",
-                              "}()"].join("");
-
-                return '<a href="#" class="pager_link" onclick="' + lambda + '">' + page_num + '</a>';
-              }
-          }).join(" | ")
-        );
-        
       });
     return false;
   });
 });
+
+
+var epi = {
+  page_to: function(page) {
+    $('#search_form')[0].skip.value = page * $('#search_form')[0].limit.value;
+    $('#search_form').trigger('submit');
+    return false;  
+  },
+  
+  search_results_metadata: function(data) {
+    return $.mustache(
+      "Results {{from}} - {{to}} of {{total_rows}} ({{duration}} seconds)",
+      {
+        from:       data.skip + 1,
+        to:         Math.min(data.skip + data.limit, data.total_rows),
+        total_rows: data.total_rows,
+        duration:   ((data.search_duration 
+                        + data.fetch_duration) / 1000).toFixed(3)
+      }
+    )
+  },
+  
+  search_results: function(data) {
+    return $.mustache(
+      "{{#rows}} <div>{{timestamp}} - {{default}}</div> {{/rows}}",
+      {rows: _(data.rows).map( function(row) { return row.fields } )}
+    )
+  },
+  
+  search_pager: function(data) {
+    var num_pages = Math.ceil(data.total_rows / data.limit);
+    var current_page = parseInt(data.skip / data.limit) + 1;
+    
+    return _(_.range(num_pages)).map(function(i) {
+        var page_num = i + 1;
+
+        if (page_num == current_page) {
+          return page_num;
+
+        } else {
+          return $.mustache(
+            '<a href="#" class="pager_link" onclick="return epi.page_to({{index}})">{{page_num}}</a>', 
+            {"page_num": page_num, "index": i}
+          );
+
+        }
+    }).join(" | ")
+  }
+}
+
+
+
